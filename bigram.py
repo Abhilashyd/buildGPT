add self-attention head, fix training loop indenttion, fix softmax dim

bigram.py
@@ -136,15 +136,16 @@ optimizer=torch.optim.AdamW(m.parameters(),lr=learning_rate)
 
 for iter in range(max_iters):
 
-    #evaluate loss eevery eeval_interval steps
+    #evaluate loss every eval_interval steps
     if iter % eval_interval==0:
         losses=estimate_loss()
         print(f"step{iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
-        xb,yb=get_batch('train')
-        logits,loss=model(xb,yb)#find loss
-        optimizer.zero_grad(set_to_none=True)#backward pass
-        loss.backward()
-        optimizer.step()
+
+    xb,yb=get_batch('train')
+    logits,loss=model(xb,yb)#find loss
+    optimizer.zero_grad(set_to_none=True)#backward pass
+    loss.backward()
+    optimizer.step()
 
 # generate
 context = torch.zeros((1, 1), dtype=torch.long, device=device)